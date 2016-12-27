2a7d44e486d9052edd075456ce08d72d8b6a9aac
Nelson Dias <nelson.dias@polarising.com>
2016-12-27 17:38:50 +0000

    updated m2 code challenge with draft Service

--- angularJS/Module 2/m2CodeChallenge/js/app.js
+++ angularJS/Module 2/m2CodeChallenge/js/app.js
@@ -13,11 +13,78 @@
 
 
 angular.module('ShoppingListCheckOff', [])
-.controller("ToBuyController", ToBuyCtrl)
-.controller("AlreadyBoughtController", AlrBoughtCtrl);
+.controller('ToBuyController', ToBuyCtrl)
+.controller("AlreadyBoughtController", AlrBoughtCtrl)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    //Buy List Controller
+    ToBuyController.$inject = ['ShoppingListCheckOffService']; //inject the service in the controller
+    function ToBuyCtrl(ShoppingListCheckOffService) {
+        /* as a Controller as Syntax, we are attaching all properties directly to the instance of the controller
+         that is automatically attached to $scope for us*/
+        var itemAdder = this;
+
+        itemAdder.itemName = "";
+        itemAdder.itemQuantity = "";
+
+        itemAdder.items = ShoppingListCheckOffService.getBuyItems();
+
+        itemAdder.addItem = function () {
+            ShoppingListCheckOffService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+        };
+    }
+
+    //Already Bought Controller
+    AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"]; // inject the service in the controller
+    function AlrBoughtCtrl(ShoppingListCheckOffService) {
+        var showList = this;
+
+        showList.items = ShoppingListCheckOffService.getBoughtItems();
+
+        showList.removeItem = function (itemIndex) {
+            ShoppingListCheckOffService.removeItem(itemIndex);
+        };
+    }
 
 
+//function constructor Service
+    function ShoppingListCheckOffService(){
+        /* as a Controller as Syntax, we are attaching all properties directly to the instance of the controller
+         that is automatically attached to $scope for us*/
+        var service = this;
 
+        // List of items to buy
+        var toBuyItems = [{name: "Cookies", quantity: 10},
+            {name: "Cookies", quantity: 10},
+            {name: "Cookies", quantity: 10},
+            {name: "Cookies", quantity: 10},
+            {name: "Cookies", quantity: 10},
+            {name: "Cookies", quantity: 10}];
+
+        var boughtItems = [];
+
+        service.addItem = function (itemName, itemQuantity) { // service method responsible to add elements to Shopping List
+            var item = {
+                name: itemName,
+                quantity: itemQuantity
+            };
+            toBuyItems.push(item);
+        };
+
+        service.removeItem = function (itemIndex) { // service method responsible to remove elements of Shopping List
+            var ItemBought = 34;
+            boughtItems.splice(ItemBought);
+            toBuyItems.splice(itemIndex, 1);
+        };
+
+        service.getBuyItems = function () { // service method responsible to show all elements in Shopping List
+            return toBuyItems;
+        };
+
+        service.getBoughtItems = function () { // service method responsible to show all elements in Shopping List
+            return boughtItems;
+        };
+    }
 
 
 })();
