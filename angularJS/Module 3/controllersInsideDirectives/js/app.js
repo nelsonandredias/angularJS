3d8d2e8cd3552c134e1b1eaa4a54e5c55644afb4
Nelson Dias <nelson.dias@polarising.com>
2017-01-06 10:39:37 +0000

    added simple example about controllers inside Directives

---
+++ angularJS/Module 3/controllersInsideDirectives/js/app.js
@@ -0,0 +1,124 @@
+/**
+ * Created by nelson.dias on 25/11/2016.
+ */
+// IIFE definition
+/*we want to make sure that no local variables bleed into the global scope
+* */
+
+(function(){
+
+//best practice for protecting us from making mistakes
+'use strict';
+
+angular.module("ShoppingListApp", [])
+
+.controller("ShoppingListController", ShoppingListController)
+.factory("ShopListFactory", ShoppingListFactory)
+// declaration if want to use a controller in more than one directive (re-use)
+.controller("ShoppingListDirectiveController", ShoppingListDirectiveController)
+.directive("shoppingList", ShoppingListDirective); //directive to display items and quantities in ng-repeat
+
+    //factory directive function - to display item description
+    function ShoppingListDirective() {
+        var ddo = {
+            templateUrl: "shoppingList.html",
+            scope: {
+                    items: "<", // one way-binding because we don't want to change anything about the items
+                    title: "@"
+            },
+            controller: ShoppingListDirectiveController,
+            bindToController: true, // tell angular we want to bind all of the scope variables to our controller
+            controllerAs: "list", // label used inside shoppingList.html
+            //if we want to use a controller in this directive but also somewhere else:
+            //controller: "ShoppingListDirectiveController as list",
+            //bindToController: true,
+        };
+        return ddo;
+    }
+
+    // Directive Controller
+    function ShoppingListDirectiveController(){
+        var list = this;
+
+        //if there is a cookie in the shooping list, display a warning
+        // this functionality is just inside of our controller
+        list.cookiesInList = function(){
+            for (var i=0; i < list.items.length; i++){
+                var name = list.items[i].name;
+                if(name.toLowerCase().indexOf("cookie") !== -1){
+                    return true;
+                }
+            }
+
+            return false;
+        };
+
+    }
+
+
+    //List 1 - Controller
+    ShoppingListController.$inject = ["ShopListFactory"]; //inject the factory in the controller
+    function ShoppingListController(ShoppingListFactory) {
+        var list1 = this;
+
+        //User factory to create a new Shopping List Service
+        var shoppingList = ShoppingListFactory();
+
+        list1.items = shoppingList.getItems();
+
+        list1.itemName = "";
+        list1.itemQuantity = "";
+
+        list1.addItem = function () {
+            shoppingList.addItem(list1.itemName, list1.itemQuantity);
+        }
+
+        list1.removeItem = function (itemIndex) {
+            shoppingList.removeItem(itemIndex);
+        };
+    }
+
+
+    //function constructor Service that it will be produced in factory function
+    function ShoppingListService(maxItems){
+        /* as a Controller as Syntax, we are attaching all properties directly to the instance of the controller
+        that is automatically attached to $scope for us*/
+        var service = this;
+
+        var items = []; // List of shopping items
+
+        service.addItem = function (itemName, itemQuantity) { // service method responsible to add elements to Shopping List
+
+          if((maxItems === undefined) || (maxItems !== undefined) && (items.length < maxItems)){
+            var item = {
+                name: itemName,
+                quantity: itemQuantity
+            };
+            items.push(item);
+          }else{
+            throw new Error("Max Items ("+ maxItems +") reached.");
+          }
+
+        };
+
+        service.removeItem = function (itemIndex) { // service method responsible to remove elements of Shopping List
+            items.splice(itemIndex, 1);
+        };
+
+        service.getItems = function () { // service method responsible to show all elements in Shopping List
+            return items;
+        };
+    }
+
+    /*
+     We do not want Angular to create the service for us...we want to create the service ourselves*/
+    function ShoppingListFactory() {
+        var factory = function (maxItems) { // factory function that creates a service "ShoppingListService"
+          return new ShoppingListService(maxItems);
+        };
+
+        return factory;
+    }
+
+
+})();
