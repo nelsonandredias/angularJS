8a5a26a28f304eb537ea4acb536ee575ecfc968e
Nelson Dias <nelson.dias@polarising.com>
2017-01-19 10:16:01 +0000

    added retrieve of results if searched string appears in description

--- angularJS/Module 3/m3CodeChallenge/js/app.js
+++ angularJS/Module 3/m3CodeChallenge/js/app.js
@@ -13,6 +13,7 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
+/*.service('getDescriptionItemService', getDescriptionItemService)*/
 .constant("ApiBasePath","http://davids-restaurant.herokuapp.com");// constant function that's evoked everytime on the angular module
 
     //Narrow List Controller
@@ -26,23 +27,18 @@ angular.module('NarrowItDownApp', [])
 
 
         narrow.itemSubmit = function () {
-
             try{
 
                 var promise = MenuSearchService.getMatchedMenuItems(narrow.itemName);
 
                 promise.then(function (response) {
-
-                    /*narrow.list = response.data;*/
-                    return WeightLossFilterService.getMenuDescriptions(narrow.itemName,response.data);
-
-                })
-                .then(function (response) {
-
                     narrow.list = response;
+                    narrow.errorMessage = "";
+
                 })
                 .catch(function (error) {
-                        console.log("Something went terribly wrong.");
+                    narrow.list = "";
+                    narrow.errorMessage = error.message;
                 });
 
             }catch(error){
@@ -61,25 +57,51 @@ angular.module('NarrowItDownApp', [])
 
 
     //function constructor Service
-    MenuSearchService.$inject = ["$http", "ApiBasePath", "$q"]; //inject the HTTP service and the constant function
+    MenuSearchService.$inject = ["$http", "ApiBasePath"]; //inject the HTTP service and the constant function
     function MenuSearchService($http, ApiBasePath){
         /* as a Controller as Syntax, we are attaching all properties directly to the instance of the controller
          that is automatically attached to $scope for us*/
         var service = this;
-        var found = [];
-        var objDescription = {};
 
-        //service method "getMatchedMenuItems" to reach out to the server and retrieve the list of all the menu items
+
+        //service method "getMatchedMenuItems" to reach out to the server and retrieve the list of raw menu items data
         service.getMatchedMenuItems = function (itemName) {
 
             if(itemName.length > 0 || itemName === undefined){ // check if string is empty
-                // response will capture the promise
-                var response = $http({
+
+                var accessDB = $http({
                     method: "GET",
-                    url: (ApiBasePath + "/menu_items.json"),
+                    url: (ApiBasePath + "/menu_items.json")
                 });
-                // return the promise
-                return response;
+
+                // as the $http service returns a promise we can invoke the "then" method
+                return accessDB.then(function success (response) {
+
+                        var array = response.data.menu_items;
+                        var found = [];
+                        var objDescription = {};
+
+                        for (var i = 0; i < array.length; i ++){
+
+                            var descriptionString = array[i].description;
+                            // check if string searched appears anywhere in the description of the item
+                            if(descriptionString.search(itemName) > 0 ){
+                                objDescription = {"shortName": array[i].short_name, "name": array[i].name, "description": array[i].description};
+                                found.push(objDescription);
+                            }
+
+                        }
+
+                        if(found.length > 0){ // if found, return results
+                            return found;
+                        }else{
+                            throw new Error("Nothing found");
+                        }
+
+                  }, function error (errorResponse) {
+                        console.log(errorResponse.message);
+                  });
+
 
             }else{
                 throw new Error("Nothing found");
@@ -87,33 +109,11 @@ angular.module('NarrowItDownApp', [])
 
         };
 
-
-        //service method to get Menu Descriptions based on itemName typed in textbox
-        service.getMenuDescriptions = function (itemName, menuItems) {
-
-            /*var array = menuItems["menu_items"];
-
-            for (var i = 0; i < array.length; i ++){
-                var descriptionString = array[i].description;
-                if(descriptionString.search(itemName) > 0 ){
-                    objDescription = {"shortName": array[i].short_name, "name": array[i].name, "description": array[i].description};
-                    found.push(objDescription);
-                }
-            }
-
-            return found;*/
-            return menuItems;
-        };
-
         // service method responsible to remove elements of Shopping List
         service.removeItem = function (itemIndex) {
             found.splice(itemIndex, 1);
         };
 
-
     }
-
-
-
 
 })();
