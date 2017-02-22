ceb949fcb9968b1251b058d636e4e544a5cc63ee
Nelson Dias <nelson.dias@polarising.com>
2017-02-22 10:01:23 +0000

    defined routing states

---
+++ angularJS/Module 4/m4CodeChallenge/src/restaurantList/js/shoppingList.module.js
@@ -0,0 +1,11 @@
+/**
+ * Created by nelson.dias on 14/02/2017.
+ */
+
+(function () {
+    'use strict';
+
+    // we are importing the ui-router
+    angular.module('MenuApp', ['ui.router']);
+
+})();
